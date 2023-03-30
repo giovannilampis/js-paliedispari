86a1ef101b8c6447e0c8fdb85e24beb789fc7a48
giovannilampis <giolampis@hotmail.com>
2023-03-30 16:41:07 +0200

    add basic variables to script.js

--- js/script.js
+++ js/script.js
@@ -54,9 +54,23 @@ checkWord.addEventListener("click", function() {
 
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 
+// get the select element in html
 
+const chooseOddEven = document.getElementById("choose-odd-even");
+
+// get the input element in html
+
+const launch = document.getElementById("launch");
+
+// get the #result-odd-even element inhtml
+
+const resultOddEven = document.getElementById("result-odd-even");
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+const play = document.getElementById("play");
+
+
 
 
 
