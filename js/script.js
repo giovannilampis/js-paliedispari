f6ac8f81646664bfb391b7c4bb51d10fc0a06379
giovannilampis <giolampis@hotmail.com>
2023-03-30 15:28:23 +0200

    add basic variables to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict"
+
+const typeWord = document.getElementById("type-word");
+
+const checkWord = document.getElementById("check-word");
+
+const result = document.getElementById("result");
+
