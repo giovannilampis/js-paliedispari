dea38a60a33fe8f9e3637824dce665a79f429273
giovannilampis <giolampis@hotmail.com>
2023-03-30 15:57:51 +0200

    add content to script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
 "use strict"
+// get the #type-word element in html
 
 const typeWord = document.getElementById("type-word");
 
+// get the #check-word element in html
+
 const checkWord = document.getElementById("check-word");
+
+// get the #result element in html
 
 const result = document.getElementById("result");
 
+// create a function that can verify wether a word is a palindrome or not
+function truePalindrome(word) {
+
+    // create a word matching the entered word but reversed
+    const invertedWord = String(word).toLowerCase().trim().split("").reverse().join("");
+
+    // if the reversed word is identical to the one entered ...
+    if(word.toLowerCase() == invertedWord)
+
+    // ... then the entered word is a true palindrome
+    return true
+
+    // otherwise it is not
+    else return false
+
+}
+
+// ADD EVENT LISTENER
+
+checkWord.addEventListener("click", function() {
+
+    const verify = truePalindrome( typeWord.value );
+
+    if (verify === true) {
+        result.innerHTML = `La parola ${typeWord.value} è un vero palindromo 😍`;
+    } else {
+        result.innerHTML = `La parola inserita non è un palindromo 😭`;
+    }
+})
